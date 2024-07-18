e5ce328250c64d8b5840b54d13e87df74c76689d
Ryan Duer <ryan@allmaddesigns.com>
2024-07-18 12:31:48 -0700

    hmm

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -72,7 +72,7 @@ module.exports = defineConfig({
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: 'npm run start',
-		url: 'http://127.0.0.1:3000',
-		reuseExistingServer: !process.env.CI,
+		url: 'http://localhost:3000',
+    reuseExistingServer: true
 	},
 });
