5ff49c0f5143530bbf83f318339b0c56e94c0816
Ryan Duer <ryan@allmaddesigns.com>
2024-07-18 18:39:41 -0700

    create before hooks and add db context

--- tests/user.spec.cjs
+++ tests/user.spec.cjs
@@ -2,20 +2,21 @@
 const { test, expect } = require('@playwright/test');
 
 // Ony Run These Tests if database has user 
-
-test('Ensure user page loads', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-// Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/JS Job Helper/);
-  await page.getByRole('heading', {name: 'Your Info'}).click()
-  await expect(page.getByRole('heading', {name: 'Your Info'})).toBeInViewport()
-
-});
-
 test.describe.serial('Run these tests in order', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.reload();
+    await page.waitForSelector('[id="add-links-button"]', { timeout: 30000})
+  });
+  test('Ensure user page loads', async ({ page }) => {
+  // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/JS Job Helper/);
+    await page.getByRole('heading', {name: 'Your Info'}).click()
+    await expect(page.getByRole('heading', {name: 'Your Info'})).toBeInViewport()
+  
+  });
   // run test with database existing user
   test('run when you have a user in the database', async ({ page }) => {
-    await page.goto('http://localhost:3000');
     await page.waitForSelector('[data-testid="full-name"]', { timeout: 30000})
     await page.getByTestId('full-name').click();
     await expect(page.getByTestId('full-name')).not.toBeEmpty()
@@ -23,8 +24,6 @@ test.describe.serial('Run these tests in order', () => {
   
   // Test links interface
   test('to add new link, fill, and save', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.waitForSelector('[id="add-links-button"]', { timeout: 30000})
     await page.getByRole('button', {name: 'Add Links?'}).click()
     await page.getByTestId('title-0').click()
     await expect(page.getByTestId('title-0')).toBeEmpty()
@@ -40,9 +39,6 @@ test.describe.serial('Run these tests in order', () => {
     await expect(page.getByTestId('href-0')).not.toBeInViewport()
   })
   test('find existing link, delete link, and save', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.reload();
-    await page.waitForSelector('[id="add-links-button"]', { timeout: 30000})
     await page.getByRole('button', {name: 'Add Links?'}).scrollIntoViewIfNeeded()
     await page.getByPlaceholder('fake.com', { exact: false }).click()
     await expect(page.getByPlaceholder('fake.com', { exact: false })).toBeInViewport()
@@ -51,9 +47,6 @@ test.describe.serial('Run these tests in order', () => {
     await expect(page.getByPlaceholder('fake.com', { exact: false })).not.toBeInViewport()
   })
   test('add a new link then delete it', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.reload();
-    await page.waitForSelector('[id="add-links-button"]', { timeout: 30000})
     await page.getByRole('button', {name: 'Add Links?'}).click()
     await page.getByTestId('title-0').fill('Fake')
     await expect(page.getByTestId('title-0')).not.toBeEmpty()
