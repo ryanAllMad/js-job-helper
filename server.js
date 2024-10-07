471b37872e6543a019fbd3354b04ba6a3b8beb8e
Ryan Duer <ryan@allmaddesigns.com>
2024-10-07 12:43:54 -0700

    add google docs integration

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ import { userRouter } from './routes/userRoutes.js'
 import { requirementsRouter } from './routes/requirementRoutes.js';
 import { jobPostRouter } from './routes/jobPost.js';
 import { resumeRouter } from './routes/resumeRoutes.js';
+import path from 'path'
+
+import  {authenticate} from '@google-cloud/local-auth'
+import  {google} from 'googleapis'
 
 // Constants
 const isProduction = process.env.NODE_ENV === 'production'
@@ -86,6 +90,90 @@ app.use('*', async (req, res) => {
     res.status(500).end(e.stack)
   }
 })
+
+
+// Google Docs API Info
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/documents.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Prints the title of a sample doc:
+ * https://docs.google.com/document/d/195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth 2.0 client.
+ */
+async function printDocTitle(auth) {
+  const docs = google.docs({version: 'v1', auth});
+  const res = await docs.documents.get({
+    documentId: '195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE',
+  });
+  console.log(`The title of the document is: ${res.data.title}`);
+}
+
+authorize().then(printDocTitle).catch(console.error);
+
+// 1. figure out how to create a blank doc
+// 2. figure out how to send resume info formatted as a google doc
+// 3. figure out how to run the create doc code on a button press
+
 
 // Start http server
 app.listen(port, () => {
